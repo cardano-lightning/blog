5ccac7de0eeff52d6f94904cf818ccfefa8ab318
paluh (aka Tomasz Rybarczyk) <paluho@gmail.com>
2024-12-04 21:59:56 +0100

    Drop cusdis dependency

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['theme/index.tsx', 'theme/cusdis.tsx', 'theme/tags.tsx'],
+  entry: ['theme/index.tsx', 'theme/tags.tsx'],
   format: 'esm',
   dts: true,
   name: 'cardano-lightning-network-theme',
